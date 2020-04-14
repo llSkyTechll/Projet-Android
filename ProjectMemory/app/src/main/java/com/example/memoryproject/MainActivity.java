898c7157dc1ef45757f723bf1bf762d518586efc
Alexandre Reny <areny1998@hotmail.com>
2020-04-14 11:08:33 -0400

    Main menu

--- ProjectMemory/app/src/main/java/com/example/memoryproject/MainActivity.java
+++ ProjectMemory/app/src/main/java/com/example/memoryproject/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.memoryproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btn_play);
+
+        setListener();
+    }
+
+    private void setListener(){
+        btnPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                relocateSelectGrid();
+            }
+        });
+    }
+
+    private void relocateSelectGrid(){
+        setContentView(R.layout.grid_choice);
     }
 }
