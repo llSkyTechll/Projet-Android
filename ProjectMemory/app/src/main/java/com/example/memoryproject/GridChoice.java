4bb432a5233f0da713fe8cde268384197b3bb9c4
Alexandre Reny <areny1998@hotmail.com>
2020-04-20 09:01:09 -0400

    Choix de grilles

--- ProjectMemory/app/src/main/java/com/example/memoryproject/GridChoice.java
+++ ProjectMemory/app/src/main/java/com/example/memoryproject/GridChoice.java
@@ -3,13 +3,18 @@ package com.example.memoryproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Picture;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class GridChoice extends AppCompatActivity {
 
     Button btn_2x2;
+    Button btn_4x4;
+    Button btn_6x6;
+    int picturesRequired = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,8 @@ public class GridChoice extends AppCompatActivity {
         setContentView(R.layout.activity_grid_choice);
 
         btn_2x2 = findViewById(R.id.btn_2x2);
-
+        btn_4x4 = findViewById(R.id.btn_4x4);
+        btn_6x6 = findViewById(R.id.btn_6x6);
         setListener();
     }
 
@@ -25,13 +31,35 @@ public class GridChoice extends AppCompatActivity {
         btn_2x2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                relocateGrid2x2();
+                picturesRequired = 2;
+                relocatePictureChoice();
+            }
+        });
+
+        btn_4x4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                picturesRequired = 8;
+                relocatePictureChoice();
+            }
+        });
+
+        btn_6x6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                picturesRequired = 18;
+                relocatePictureChoice();
             }
         });
     }
 
-    private void relocateGrid2x2(){
-        //Intent grid2x2Intent = new Intent(this, /*2x2 Grid page?*/);
-        //startActivity(grid2x2Intent);
+    private void relocatePictureChoice(){
+        /*Intent pictureChoiceIntent = new Intent(this, PictureChoice.class);
+        pictureChoiceIntent.putExtra("picturesRequired", picturesRequired);
+        startActivity(pictureChoiceIntent);*/
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(GridChoice.this, message, Toast.LENGTH_SHORT).show();
     }
 }
