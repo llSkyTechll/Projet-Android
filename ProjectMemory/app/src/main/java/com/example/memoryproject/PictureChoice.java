d7795031a243a9969fbf662a925c3f9a8f883101
Johnatan Fortier-Roy <demon.a.queue@gmail.com>
2020-04-24 11:08:21 -0500

    Pick Multiple Image

--- ProjectMemory/app/src/main/java/com/example/memoryproject/PictureChoice.java
+++ ProjectMemory/app/src/main/java/com/example/memoryproject/PictureChoice.java
@@ -1,82 +1,123 @@
 package com.example.memoryproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PictureChoice extends AppCompatActivity {
-
-    private static int RESULT_LOAD_IMAGE = 1;
-    private static int RESULT_PERMISSION_CAMERA = 100;
-    Button buttonLoadImage;
-    Button btn_takePicture;
+    Button btnPickImage;
     ImageView imageView;
-    Uri selectedImage;
-
+    List<Bitmap> bitmaps;
+    ClipData clipData;
+    Uri imageUri;
+    InputStream inputStream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_choice);
-
-        imageView = (ImageView)findViewById(R.id.imgView);
-        buttonLoadImage = findViewById(R.id.buttonLoadPicture);
-        btn_takePicture = findViewById(R.id.btn_takePicture);
+        btnPickImage = findViewById(R.id.btn_pickImage);
         setListener();
-    }
 
+    }
     private void setListener(){
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+        btnPickImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 permission();
-                openGallery();
-            }
-        });
-
-        btn_takePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requestCameraPermission();
+                intentContent();
             }
         });
     }
 
-    private void requestCameraPermission(){
-        if (ContextCompat.checkSelfPermission(this , Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this , new String[] {Manifest.permission.CAMERA}, 1);
-        }
-    }
-
-    private  void permission(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RESULT_PERMISSION_CAMERA);
+    private void permission(){
+        if (ActivityCompat.checkSelfPermission(PictureChoice.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PictureChoice.this,
+                    new  String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
             return;
         }
     }
 
-    private void openGallery(){
-        Intent gallery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        gallery.setType("image/*");
-        gallery.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        gallery.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(gallery, "Select pictures"), 1);
+    private void intentContent(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+        intent.setType("image/*");
+        startActivityForResult(intent,1);
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMAGE) {
-            selectedImage = data.getData();
-            imageView.setImageURI(selectedImage);
+        getImages(requestCode, resultCode, data);
+        imageThread();
+    }
+
+    private void getImages(int requestCode, int resultCode, @Nullable Intent data){
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            imageView = findViewById(R.id.imageView);
+            bitmaps = new ArrayList<>();
+            clipData = data.getClipData();
+            if (clipData !=null){
+                for (int i =0;i<clipData.getItemCount();i++){
+                    imageUri =clipData.getItemAt(i).getUri();
+                    try {
+                        inputStream = getContentResolver().openInputStream(imageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        bitmaps.add(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else {
+                imageUri = data.getData();
+                try {
+                    inputStream =getContentResolver().openInputStream(imageUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    bitmaps.add(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    private void imageThread(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (final Bitmap b :bitmaps) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(b);
+                        }
+                    });
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
