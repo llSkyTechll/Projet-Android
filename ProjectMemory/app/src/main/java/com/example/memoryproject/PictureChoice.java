25fcea115fb318297fbd929b985a944bef7a9fed
Johnatan Fortier-Roy <demon.a.queue@gmail.com>
2020-04-27 09:25:19 -0500

    take image from camera

--- ProjectMemory/app/src/main/java/com/example/memoryproject/PictureChoice.java
+++ ProjectMemory/app/src/main/java/com/example/memoryproject/PictureChoice.java
@@ -1,17 +1,21 @@
 package com.example.memoryproject;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.ClipData;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -26,13 +30,17 @@ public class PictureChoice extends AppCompatActivity {
 
     Button btnPickImage;
     Button btnStartGame;
+    Button btnCapture;
     ImageView imageView;
     List<Bitmap> bitmaps;
     ClipData clipData;
     Uri imageUri;
+    private static final int PERMISSION_CODE =1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     InputStream inputStream;
     Intent intent;
     int picturesRequired;
+    boolean isCaptured = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,8 @@ public class PictureChoice extends AppCompatActivity {
         setContentView(R.layout.activity_picture_choice);
         btnPickImage = findViewById(R.id.btn_pickImage);
         btnStartGame = findViewById(R.id.btn_goToGame);
+        btnCapture =findViewById(R.id.btn_takeImage);
+        imageView = findViewById(R.id.imageView);
         intent = getIntent();
         picturesRequired = intent.getIntExtra("picturesRequired",2);
         bitmaps = new ArrayList<>();
@@ -50,6 +60,7 @@ public class PictureChoice extends AppCompatActivity {
         btnPickImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                isCaptured = false;
                 permission();
                 intentContent();
             }
@@ -61,6 +72,44 @@ public class PictureChoice extends AppCompatActivity {
                 startGame();
             }
         });
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCaptured = true;
+                permissionCamera();
+            }
+        });
+    }
+
+    private  void permissionCamera(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                requestPermissions(permission,PERMISSION_CODE);
+            } else {
+                openCamera();
+            }
+        }else {
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
+        ContentValues values =new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the camera");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        intentCamera();
+    }
+
+    private void intentCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
     }
 
     private void startGame(){
@@ -87,16 +136,36 @@ public class PictureChoice extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    openCamera();
+                } else {
+                    showMessage("Permission denied...");
+                }
+        }
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        getImages(requestCode, resultCode, data);
-        imageThread();
+        if (isCaptured){
+            setImage(resultCode);
+        } else {
+            getImages(requestCode, resultCode, data);
+            imageThread();
+        }
+    }
+    private void setImage(int resultCode){
+        if (resultCode == RESULT_OK){
+            imageView.setImageURI(imageUri);
+        }
     }
 
     private void getImages(int requestCode, int resultCode, @Nullable Intent data){
         if (requestCode == 1 && resultCode == RESULT_OK){
-            imageView = findViewById(R.id.imageView);
-
             clipData = data.getClipData();
             if (clipData !=null){
                 for (int i = 0; i < clipData.getItemCount();i++){
